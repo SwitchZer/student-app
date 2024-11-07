266a2285d09c431d0e5e0446ff4a2a5790c5a1e0
SwitchZer <alfredzagoto21@gmail.com>
2024-11-07 18:06:22 +0700

    develop branch

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
-};
+import { Calendar } from "./components/Calendar";
+import { WellbeingRecords } from "./components/WellbeingRecords";
+import { Header } from "./components/Header";
 
-export default page;
+export default function StudentDashboard() {
+  return (
+    <div className="flex-1 p-6">
+      <Header date="Friday, 19 June 2024" />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <Calendar onDateSelect={(date) => console.log(date)} />
+        <WellbeingRecords />
+      </div>
+    </div>
+  );
+}
